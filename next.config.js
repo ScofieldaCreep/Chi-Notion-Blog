39d4c0fcc00b455078afe2d44509dc30f34d6f8e
ScofieldaCreep <tju_scofield@outlook.com>
2024-09-25 01:26:36 +0800

    Debug

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-  basePath: '/blog',
   staticPageGenerationTimeout: 300,
   images: {
     domains: [
